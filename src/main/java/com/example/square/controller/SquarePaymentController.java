86f74571198ae047a80d8e896ec2adfa3ca4e126
Javed Bagwan <javed460@gmail.com>
2023-12-19 08:17:48 +0530

    Updated SquarePaymentController

--- src/main/java/com/example/square/controller/SquarePaymentController.java
+++ src/main/java/com/example/square/controller/SquarePaymentController.java
@@ -1,6 +1,10 @@
 package com.example.square.controller;
 
+import com.example.square.dto.AmountDTO;
+import com.example.square.service.SquarePaymentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/square")
 @RequiredArgsConstructor
 public class SquarePaymentController {
+    private final SquarePaymentService squarePaymentService;
+
+    @PostMapping("/create-payment")
+    public String createPayment(@RequestBody AmountDTO amountDTO) {
+        return squarePaymentService.createPayment(amountDTO.getAmount());
+    }
     
 }
